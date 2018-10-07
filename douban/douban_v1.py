e5043ccffdb098a80ed65010433c6d38806ea7a0
Amauri0508 <18206218863@163.com>
2018-10-07 15:16:37 +0800

    douban

--- douban/douban_v1.py
+++ douban/douban_v1.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import re
 import numpy as np
 import csv
+import time
 
 
 # 获取内容
@@ -10,30 +11,33 @@ def get(page):
     url = 'https://book.douban.com/tag/%E7%BC%96%E7%A8%8B?start={}&type=T'
     offset = 0
     result = []
-    err_url = []
     for i in range(page):
         _url = url.format(offset)
         offset += 20
         resp = requests.get(_url)
         if resp.status_code == 200:
             html = resp.text
-            bs4 = BeautifulSoup(html, "lxml")
-            book_info_list = bs4.find_all('li', class_='subject-item')
-            for book_info in book_info_list:
-                list_ = []
-                star = book_info.find('span', class_='rating_nums').get_text()
-                if float(star) < 9.0:
-                    continue
-                title = book_info.find('h2').get_text().replace(' ', '').replace('\n', '')
-                comment = book_info.find('span', class_='pl').get_text()
-                comment = re.sub("\D", "", comment)
-                list_.append(title)
-                list_.append(comment)
-                list_.append(star)
-                result.append(list_)
-        else:
-            err_url.append(_url)
-    return result, err_url
+            if html is not None:
+                bs4 = BeautifulSoup(html, "lxml")
+                book_info_list = bs4.find_all('li', class_='subject-item')
+                if book_info_list is not None:
+                    for book_info in book_info_list:
+                        list_ = []
+                        try:
+                            star = book_info.find('span', class_='rating_nums').get_text()
+                            if float(star) < 9.0:
+                                continue
+                            title = book_info.find('h2').get_text().replace(' ', '').replace('\n', '')
+                            comment = book_info.find('span', class_='pl').get_text()
+                            comment = re.sub("\D", "", comment)
+                            list_.append(title)
+                            list_.append(comment)
+                            list_.append(star)
+                            result.append(list_)
+                        except:
+                            continue
+    time.sleep(1)
+    return result
 
 
 # 根据评分排序
@@ -55,12 +59,14 @@ def save(data):
     header = ['书名', '评论数', '评分']
     writer.writerow(header)
     for book in data:
-        writer.writerow(book)
+        try:
+            writer.writerow(book)
+        except:
+            continue
     file_csv.close()
 
 
 if __name__ == '__main__':
-    result, err_url = get(10)
+    result = get(96)
     sort_list = _sort(result)
     save(sort_list)
-    print(err_url)
